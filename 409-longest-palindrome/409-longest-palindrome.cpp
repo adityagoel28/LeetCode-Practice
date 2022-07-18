0e57fa623d02eedb2a8ec3080ed49e023a6e0aad
Aditya Goel <adityagoel2002@gmail.com>
2022-07-18 19:54:04 +0530

    Time: 4 ms (64.29%), Space: 6.7 MB (14.00%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -3,45 +3,51 @@ public:
     int longestPalindrome(string s) {
        // Basic Solution (not good) 
         
-        int len = 0, k = 0, j = 0;
-        unordered_map<char, int> M;
-        for(int i = 1; i<s.length(); i++){
-            if(s[i] != s[i-1]){
-                j = 1;
+        // int len = 0, k = 0, j = 0;
+        // unordered_map<char, int> M;
+        // for(int i = 1; i<s.length(); i++){
+        //     if(s[i] != s[i-1]){
+        //         j = 1;
+        //         break;
+        //     }
+        // }
+        // if(j == 0)  return s.length();
+        // for (int i = 0; s[i]; i++){
+        //     // If the current characters is not found then insert
+        //     // current characters with frequency 1
+        //     if(M.find(s[i]) == M.end()){
+        //         M.insert(make_pair(s[i], 1));
+        //     }
+        //     else    M[s[i]]++;
+        // }
+        // for(auto& it : M){
+        //     if(it.second / 2 >= 1){
+        //         if(it.second % 2 != 0)
+        //             k = 1;
+        //         len += (it.second / 2) * 2;
+        //     }
+        //     else    k = 1;
+        // }
+        // if(k == 0)    return len;
+        // return len + 1;
+        
+        // Optimised solution
+        unordered_map<char,int> m;
+        int ans = 0;
+        
+        for(auto &c:s)
+            m[c]++;
+        for(auto &c:s){ 
+            ans += (m[c] / 2) * 2;
+            m[c] = m[c] % 2;
+        }
+        for(auto &c:s){
+            if(m[c] % 2 != 0){
+                ans++;
                 break;
             }
         }
-        if(j == 0)  return s.length();
-        for (int i = 0; s[i]; i++){
-            // If the current characters is not found then insert
-            // current characters with frequency 1
-            if(M.find(s[i]) == M.end()){
-                M.insert(make_pair(s[i], 1));
-            }
-            else    M[s[i]]++;
-        }
-        for(auto& it : M){
-            if(it.second / 2 >= 1){
-                if(it.second % 2 != 0)
-                    k = 1;
-                len += (it.second / 2) * 2;
-            }
-            else    k = 1;
-        }
-        if(k == 0)    return len;
-        return len + 1;
         
-        // Optimised solution
-//         unordered_map<char,int> m;
-//         int oddCount = 0, ans = 0;
-        
-//         for(auto &c:s)
-//             m[c]++;
-//         for(auto &c:s){ 
-//             ans += (m[c] / 2) * 2;
-//             m[c] = m[c] % 2;
-//         }
-        
-//         return ans;
+        return ans;
     }
 };
