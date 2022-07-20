92bcf345394cda09573f0fb3c283b1da6973d93f
Aditya Goel <adityagoel2002@gmail.com>
2022-07-20 17:43:51 +0530

    Time: 26 ms (65.47%), Space: 18 MB (18.76%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int x1 = 0, x2 = 0;
-        for(int i = 0; i<nums.size(); i++){
-            x1 = x1 ^ nums[i];
+        // Cyclic Sort Method
+        int missing = -1, i = 0, n = nums.size();
+        while(i < n){
+            int j = nums[i];
+            if(nums[i] < n && nums[i] != nums[j]){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+            else    i++;
         }
-        for(int i = 0; i<=nums.size(); i++){
-            x2 = x2 ^ i;
+        for(int k = 0; k<n; k++){
+            if(nums[k] != k) missing = k;
         }
-        cout<<x1<<x2;
-        return x1^x2;
+        if(missing == -1)    return n;
+        return missing;
+        
+        // XOR Method
+        
+        // int x1 = 0, x2 = 0;
+        // for(int i = 0; i<nums.size(); i++){
+        //     x1 = x1 ^ nums[i];
+        // }
+        // for(int i = 0; i<=nums.size(); i++){
+        //     x2 = x2 ^ i;
+        // }
+        // cout<<x1<<x2;
+        // return x1^x2;
     }
 };
