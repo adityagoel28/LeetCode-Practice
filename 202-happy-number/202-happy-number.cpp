07474ed0c6ee2ecbaa4a2377f1be6eacaf85e0ae
Aditya Goel <adityagoel2002@gmail.com>
2022-07-30 00:03:18 +0530

    Time: 4 ms (46.13%), Space: 6.4 MB (25.73%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    vector<int> summ(int num){
-        int sum = 0, count = 0;
-        vector<int> ans;
+    int summ(int num){
+        int sum = 0;
         while(num > 0){
             sum += (num % 10) * (num % 10);
             num = num/ 10;
-            count++;
         }
-        ans.push_back(sum);
-        ans.push_back(count);
-        return ans;
+        return sum;
     }
     bool isHappy(int n) {
         unordered_map<int, int> m;
         while(n != 1 && m[n] < 2){
-            vector<int> ans1 = summ(n);
-            int digit = ans1[1];
-            n = ans1[0];
+            n = summ(n);
             m[n]++;
         }
         for(auto x:m){
