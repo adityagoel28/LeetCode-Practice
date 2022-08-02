b4d95ac226c1db6a6f00955b55a57aaae0cc00ea
Aditya Goel <adityagoel2002@gmail.com>
2022-08-02 19:11:00 +0530

    Time: 186 ms (9.38%), Space: 19.3 MB (13.34%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        vector<int>::iterator it;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                // cout<<i;
+                it = nums.begin() + i;
+                nums.erase(it);
+                count++;
+                i -= 1;
+            }
+        }
+        // cout<<count;
+        for(int i = 0; i<count; i++){
+            nums.push_back(0);
+        }
+    }
+};
