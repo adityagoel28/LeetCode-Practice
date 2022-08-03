9f08b74b3fb33852b2e5ac617ee10395e319eacc
Aditya Goel <adityagoel2002@gmail.com>
2022-08-03 22:07:00 +0530

    Time: 359 ms (45.44%), Space: 124 MB (67.07%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans, pos, neg;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] > 0) pos.push_back(nums[i]);
-            if(nums[i] < 0) neg.push_back(nums[i]);
-        }
-        for(int i = 0; i<nums.size()/2; i++){
-            ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
+        vector<int> ans(nums.size()), pos, neg;
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(nums[i] > 0) pos.push_back(nums[i]);
+        //     if(nums[i] < 0) neg.push_back(nums[i]);
+        // }
+        // for(int i = 0; i<nums.size()/2; i++){
+        //     ans.push_back(pos[i]);
+        //     ans.push_back(neg[i]);
+        // }
+        
+        // Optimised Code
+        int p = 0, n = 1;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] > 0){
+                ans[p] = nums[i];
+                p += 2;
+            }
+            else{
+                ans[n] = nums[i];
+                n += 2;
+            }
         }
         return ans;
     }
