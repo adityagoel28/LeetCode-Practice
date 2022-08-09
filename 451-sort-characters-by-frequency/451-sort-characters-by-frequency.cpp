a3bd82f483b260dc1dadd8ea087676344cf35717
Aditya Goel <adityagoel2002@gmail.com>
2022-08-09 18:31:09 +0530

    Time: 24 ms (28.84%), Space: 8.5 MB (64.59%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> m;
+        string ans = "";
         for(int i = 0; i < s.length(); i++){
             m[s[i]]++;
         }
-        
-        // Priority Queue,pair<int,char
-        priority_queue<pair<int,char>>maxheap;
+        vector<pair<int, char>> v;
         for(auto it:m){
-            maxheap.push({
-                it.second,it.first
-            });
+            v.push_back({it.second, it.first});
         }
-        string ans = "";
-        while(maxheap.empty()==0){
-            pair<int,char>p = maxheap.top();
-            maxheap.pop();
-            int b = p.first;
+        sort(v.begin(), v.end());
+        for(int i = v.size()-1; i >= 0; i--){
+            int b = v[i].first;
             while(b--){
-                ans+=p.second;
+                ans += v[i].second;
             }
         }
+        // Priority Queue, pair<int,char
+        // priority_queue<pair<int,char>>maxheap;
+        // for(auto it:m){
+        //     maxheap.push({it.second,it.first});
+        // }
+        
+        // while(maxheap.empty()==0){
+        //     pair<int,char>p = maxheap.top();
+        //     maxheap.pop();
+        //     int b = p.first;
+        //     while(b--){
+        //         ans+=p.second;
+        //     }
+        // }
         return ans;
     }
 };
