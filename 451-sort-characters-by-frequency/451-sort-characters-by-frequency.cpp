37752eddd7cc1e810ce7b185a35fea38d616a925
Aditya Goel <adityagoel2002@gmail.com>
2022-08-09 16:38:40 +0530

    Time: 21 ms (39.98%), Space: 8.6 MB (23.91%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> m;
+        for(int i = 0; i < s.length(); i++){
+            m[s[i]]++;
+        }
+        vector<pair<int,char>>v;
+        for(auto it:m){
+            v.push_back({it.second,it.first});
+        }
+        sort(v.begin(),v.end());
+        string ans = "";
+        for(int i=v.size()-1;i>=0;i--){
+            int b = v[i].first;
+            while(b){
+                ans+=v[i].second;
+                b--;
+            }
+        }
+        return ans;
+    }
+};
