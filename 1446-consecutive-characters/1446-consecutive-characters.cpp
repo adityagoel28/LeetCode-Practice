cdd3cdc27354846fcb19317f40e5b0bda8179851
Aditya Goel <adityagoel2002@gmail.com>
2022-08-14 11:15:05 +0530

    Time: 6 ms (48.38%), Space: 6.8 MB (74.11%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int count = 1, maxx = 1;
+        for(int i = 0; i < s.length()-1; i++){
+            if(s[i] == s[i+1])  count++;
+            else    count = 1;
+            maxx = max(maxx, count);
+        }
+        return maxx;
+    }
+};
