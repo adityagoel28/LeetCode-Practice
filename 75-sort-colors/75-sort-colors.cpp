37089ef3c45e73f5c24afa7dda20a9ebabdfb9e9
Aditya Goel <adityagoel2002@gmail.com>
2022-08-20 11:16:51 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (23.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        map<int, int> m;
-        int j = 0;
-        for(int i = 0; i<nums.size(); i++){
-            m[nums[i]]++;
-        }
-        for(auto x:m){
-            while(x.second--){
-                nums[j] = x.first;
-                j++;
+        // map<int, int> m;
+        // int j = 0;
+        // for(int i = 0; i<nums.size(); i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto x:m){
+        //     while(x.second--){
+        //         nums[j] = x.first;
+        //         j++;
+        //     }
+        // }
+        
+        // Another approach
+        int low = 0, high = nums.size()-1, i = 0;
+        while(i <= high){
+            if(nums[i] == 0){
+                int temp = nums[i];
+                nums[i] = nums[low];
+                nums[low] = temp;
+                i++;
+                low++;
+            }
+            else if(nums[i] == 1)   i++;
+            else{
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
-//         int low = 0, high = nums.size()-1, i = 0;
-//         while(i <= high){
-            
-//         }
     }
 };
