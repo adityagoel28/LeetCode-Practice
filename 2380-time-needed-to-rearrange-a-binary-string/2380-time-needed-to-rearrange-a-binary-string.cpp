ebba3cb12ed2ef963cc98eaff444444f049de490
Aditya Goel <adityagoel2002@gmail.com>
2022-08-21 18:03:47 +0530

    Time: 242 ms (30.00%), Space: 42.8 MB (10.00%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string replace(string s){
+        for(int i = 0; i<s.size()-1; i++){
+            if(s[i] == '0' && s[i+1] == '1'){
+                s[i] = '1';
+                s[i+1] = '0';
+                i++;
+            }
+        }
+        return s;
+    }
+    int secondsToRemoveOccurrences(string s) {
+        int time = 0, n = 0, m = 1;
+        for(int i = 0; i<s.size()-1; i++){
+            if(s[i] == '0' && s[i+1] == '1'){
+                m = 0;
+            }
+        }
+        if(m == 1)  return 0;
+        while(n == 0){
+            time++;
+            s = replace(s);
+            n = 1;
+            for(int i = 0; i<s.size()-1; i++){
+                if(s[i] == '0' && s[i+1] == '1'){
+                    n = 0;
+                }
+            }
+        }
+        return time;
+    }
+};
