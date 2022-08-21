a79c3a475d0683dcfa66f2da468baaf731d5b418
Aditya Goel <adityagoel2002@gmail.com>
2022-08-21 18:15:52 +0530

    Time: 191 ms (30.00%), Space: 42.8 MB (10.00%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.cpp
@@ -3,8 +3,7 @@ public:
     string replace(string s){
         for(int i = 0; i<s.size()-1; i++){
             if(s[i] == '0' && s[i+1] == '1'){
-                s[i] = '1';
-                s[i+1] = '0';
+                swap(s[i], s[i+1]);
                 i++;
             }
         }
