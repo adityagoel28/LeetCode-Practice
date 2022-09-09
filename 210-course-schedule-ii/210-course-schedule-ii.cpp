24d40ade8cab506e5247e37595bcadfddadd35b6
Aditya Goel <adityagoel2002@gmail.com>
2022-09-09 20:17:33 +0530

    Time: 20 ms (94.76%), Space: 13.5 MB (80.82%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses, 0);
+        for(int i = 0; i<n; i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        for(int i = 0; i<n; i++){
+            indeg[prerequisites[i][1]]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++) {
+            if(indeg.at(i) == 0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()) {
+            int u = q.front(); q.pop();
+            topo.push_back(u);
+            for(int v: adj[u]) {
+                indeg[v]--;
+                if(indeg[v] == 0) q.push(v);
+            }
+        }
+        vector<int> ans;
+        reverse(topo.begin(), topo.end());
+        if(topo.size() != numCourses)    return ans;
+        return topo;
+    }
+};
