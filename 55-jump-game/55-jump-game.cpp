1ec5d87aeba42e081772c65aa6c5284be825dcac
Aditya Goel <adityagoel2002@gmail.com>
2022-09-17 13:40:51 +0530

    Time: 103 ms (62.96%), Space: 48.4 MB (44.50%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(), minJump = 0;
+        for(int i = n-2; i>=0; i--){
+            minJump++;
+            if(nums[i] >= minJump)  minJump = 0;
+        }
+        if(minJump == 0) return true;
+        return false;
+    }
+};
