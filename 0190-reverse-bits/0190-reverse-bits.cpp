a5d913e9761dbe1bae794abcb8a6244e7c991590
Aditya Goel <adityagoel2002@gmail.com>
2022-10-30 21:04:49 +0530

    Time: 4 ms (59.70%), Space: 6 MB (9.78%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t rev = 0, power = 31;
+        while(n){
+            rev += (n & 1) << power;
+            n >>= 1;
+            power -= 1;
+        }
+        return rev;
+    }
+};
