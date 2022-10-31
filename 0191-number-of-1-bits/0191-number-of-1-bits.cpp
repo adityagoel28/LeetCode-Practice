348103140e22913e17fb8667ec16e95f1a209d5e
Aditya Goel <adityagoel2002@gmail.com>
2022-10-31 10:32:42 +0530

    Time: 6 ms (28.36%), Space: 5.9 MB (47.38%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,18 @@ public:
     int hammingWeight(uint32_t n) {
         uint32_t count = 0, mask = 1;
         for(int i = 0; i<32; i++){
-            if((n & mask) != 0) count++;
-            n >>= 1; // do it like rev of bits problem
+            // 1st method
+            // if((n & mask) != 0) count++;
+            // mask <<= 1;
+            
+            // 2nd method
+            count += n % 2;
+            n /= 2;
+            
+            // 3rd method
+            // n >>= 1; // doing it like rev of bits problem
         }
+        // 4th method
         // while(n){
         //     n &= (n-1);
         //     count++;
